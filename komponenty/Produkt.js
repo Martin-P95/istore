e048fb685d8fa938b73e1e0458551cfa7fcbe221
Martin-P95 <75830632+Martin-P95@users.noreply.github.com>
2021-11-10 14:39:39 +0100

    karty

--- komponenty/Produkt.js
+++ komponenty/Produkt.js
@@ -4,9 +4,16 @@ export default function Produkt({ produkt }) {
     const { id, nazev, popis, cena } = produkt;
     return (
         <div>
-            <h2>{nazev}</h2>
-            <h3>{popis}</h3>
-            <h3>{cena}</h3>
+            <div class="card">
+                <divc class="imgBx">
+                    <img src="https://www.datart.cz/foto/250/5/9/6/product_4777695.jpg"></img>
+                </divc>
+                <div class="contentBx">
+                    <h3 class="name">{nazev}</h3>
+                    <h2 class="price">{cena}<small>kč</small></h2>
+                    <a href="#" class="buy">Koupit</a>
+                </div>
+            </div>
         </div>
     )
 }
