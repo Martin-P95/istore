4126a8c349b2741aae690bf89ac65a272216c77c
Martin-P95 <75830632+Martin-P95@users.noreply.github.com>
2021-11-15 17:28:59 +0100

    oprava chyb

--- pages/produkt/[id].js
+++ pages/produkt/[id].js
@@ -2,11 +2,21 @@ import React from "react";
 import produkty from "../../resources/produkty";
 
 export default function Produkt({ produkt }) {
-    return <div className="container">{produkt !== null ? <h1>{produkt.nazev}</h1> : <h1>Produkt nenalezen</h1>}
+    return (
+        <div >
+            {produkt !== null ? (
+                <div className="cele" >
 
 
-    </div>
 
+
+
+                </div>
+            ) : (
+                <h1>Produkt nenalezen</h1>
+            )}
+        </div>
+    );
 }
 
 export async function getServerSideProps(context) {
