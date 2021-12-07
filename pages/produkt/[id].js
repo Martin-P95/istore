9b567db5516afaf5c7b081205cbd013aac26814b
Martin-P95 <75830632+Martin-P95@users.noreply.github.com>
2021-12-07 10:24:44 +0100

    product-stranka

--- pages/produkt/[id].js
+++ pages/produkt/[id].js
@@ -1,20 +1,35 @@
 import React from "react";
 import produkty from "../../resources/produkty";
 import logo from "../../public/iStoreLogo.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faSearch, faShoppingCart, faShuttleVan } from "@fortawesome/free-solid-svg-icons";
 
 export default function Produkt({ produkt }) {
     return (
         <div >
             {produkt !== null ? (
-                <div className="cele" >
-                    <h1>{produkt.nazev}</h1>
-                    <h2>{produkt.cena} kč</h2>
-                    <img src={produkt.obrazek} alt="" />
-                    <img src={logo} alt="" />
-
-
-
+                <>
+                <div className="stranka">
+                <img className="logo"src="/iStoreLogo.svg" alt="" />
+                <div className="row">
+                  
+                <div className="col-md-6">
+                <div className="Sinfo">
+                        <h1>{produkt.nazev}</h1>
+                        <h2>{produkt.popis}</h2>
+                        <h2>{produkt.cena} kč</h2>
+                        </div>
                 </div>
+                    
+                <div className="col-md-6"> 
+                <div className="Sobrazek">
+                <img src={produkt.obrazek} alt="" />
+                </div>    
+                </div>
+                </div>
+                </div>
+                </>
+               
             ) : (
                 <h1>Produkt nenalezen</h1>
             )}
